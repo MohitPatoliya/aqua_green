823eee8c17897b0dc3a85bc22cc0249673cb3438
Mohit Patoliya <mohitpatoliya45@gmail.com>
2024-08-18 18:41:45 +0530

    add contact page1

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,12 +2,12 @@ import { Footer, Navbar } from "@/components";
 import ContactForm from "../contact-form";
 
 const Contect = () => {
-    return (
-        <>
-            <Navbar />
-            <ContactForm />
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <ContactForm />
+      {/* <Footer /> */}
+    </>
+  );
+};
 export default Contect;
